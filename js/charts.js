a9b5f58f5ba4b2afd56b1698f321c25b4a7705bc
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-02-21 18:20:22 -0800

    ENH: D3 gauge chart

--- js/charts.js
+++ js/charts.js
@@ -64,6 +64,7 @@ function buildCharts(sample) {
 
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
+    console.log(result);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otuIDs = result.otu_ids;
@@ -117,5 +118,39 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    // Deliverable 3: Gauge Chart
+    // 1. Create the trace for the gauge chart.
+    var metadata = data.metadata;
+    var metadataResultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var metadataResult = metadataResultArray[0];
+    var wfreq = parseFloat(metadataResult.wfreq);
+    var gaugeData = [{
+      value: wfreq,
+      type: "indicator",
+      mode: "gauge+number",
+      title: {text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week"},
+      gauge: {
+        axis: {range: [null, 10]},
+        bar: {color: "black"},
+        steps: [
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "yellowgreen"},
+          {range: [8, 10], color: "green"}
+        ]
+      },
+    }];
+    
+    // 2. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     width: 600,
+     height: 500,
+     margin: {t: 0, b:0}
+    };
+
+    // 3. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
